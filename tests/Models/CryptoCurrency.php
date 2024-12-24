ebcf9a535a9de47559f115546992827a3d643409
Gustavo Freze <gustavo.freze@outlook.com>
2024-12-24 12:18:21 -0300

    feat: Changes mapping implementation for array and json using tiny-blocks/mapper lib. (#20)

--- tests/Models/CryptoCurrency.php
+++ tests/Models/CryptoCurrency.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace TinyBlocks\Collection\Models;
 
-use TinyBlocks\Serializer\Serializer;
-use TinyBlocks\Serializer\SerializerAdapter;
+use TinyBlocks\Mapper\ObjectMappability;
+use TinyBlocks\Mapper\ObjectMapper;
 
-final class CryptoCurrency implements Serializer
+final class CryptoCurrency implements ObjectMapper
 {
-    use SerializerAdapter;
+    use ObjectMappability;
 
     public function __construct(public string $name, public float $price, public string $symbol)
     {
