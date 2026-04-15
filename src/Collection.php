2117e24dda48661bcbdff43e70de2a601e2bdd86
Gustavo Freze <gustavo.freze@outlook.com>
2026-04-15 15:57:05 -0300

    fix: Correct indentation in Collection

--- src/Collection.php
+++ src/Collection.php
@@ -30,145 +30,145 @@ use TinyBlocks\Mapper\IterableMapper;
 use Traversable;
 
 class Collection implements Collectible, IterableMapper
-    {
-            use IterableMappability;
+{
+    use IterableMappability;
 
     private function __construct(private readonly Pipeline $pipeline)
-        {
-        }
+    {
+    }
 
     public static function createFrom(iterable $elements): static
-        {
-                    return new static(pipeline: EagerPipeline::from(source: $elements));
-        }
+    {
+        return new static(pipeline: EagerPipeline::from(source: $elements));
+    }
 
     public static function createFromEmpty(): static
-        {
-                    return static::createFrom(elements: []);
-        }
+    {
+        return static::createFrom(elements: []);
+    }
 
     public static function createLazyFrom(iterable $elements): static
-        {
-                    return new static(pipeline: LazyPipeline::from(source: $elements));
-        }
+    {
+        return new static(pipeline: LazyPipeline::from(source: $elements));
+    }
 
     public static function createLazyFromEmpty(): static
-        {
-                    return static::createLazyFrom(elements: []);
-        }
+    {
+        return static::createLazyFrom(elements: []);
+    }
 
     public static function createLazyFromClosure(Closure $factory): static
-        {
-                    return new static(pipeline: LazyPipeline::fromClosure(factory: $factory));
-        }
+    {
+        return new static(pipeline: LazyPipeline::fromClosure(factory: $factory));
+    }
 
     public function getIterator(): Traversable
-        {
-                    yield from $this->pipeline->process();
-        }
+    {
+        yield from $this->pipeline->process();
+    }
 
     public function add(mixed ...$elements): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: Add::these(newElements: $elements)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: Add::these(newElements: $elements)));
+    }
 
     public function merge(Collectible $other): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: Merge::with(other: $other)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: Merge::with(other: $other)));
+    }
 
     public function contains(mixed $element): bool
-        {
-                    return Equality::exists(elements: $this, element: $element);
-        }
+    {
+        return Equality::exists(elements: $this, element: $element);
+    }
 
     public function count(): int
-        {
-                    return $this->pipeline->count();
-        }
+    {
+        return $this->pipeline->count();
+    }
 
     public function findBy(Closure ...$predicates): mixed
-        {
-                    return Find::firstMatch(elements: $this, predicates: $predicates);
-        }
+    {
+        return Find::firstMatch(elements: $this, predicates: $predicates);
+    }
 
     public function each(Closure ...$actions): void
-        {
-                    Each::execute(elements: $this, actions: $actions);
-        }
+    {
+        Each::execute(elements: $this, actions: $actions);
+    }
 
     public function equals(Collectible $other): bool
-        {
-                    return Equality::compareAll(elements: $this, other: $other);
-        }
+    {
+        return Equality::compareAll(elements: $this, other: $other);
+    }
 
     public function remove(mixed $element): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: Remove::element(element: $element)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: Remove::element(element: $element)));
+    }
 
     public function removeAll(?Closure $predicate = null): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: RemoveAll::matching(predicate: $predicate)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: RemoveAll::matching(predicate: $predicate)));
+    }
 
     public function filter(?Closure ...$predicates): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: Filter::matching(...$predicates)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: Filter::matching(...$predicates)));
+    }
 
     public function first(mixed $defaultValueIfNotFound = null): mixed
-        {
-                    return First::from(elements: $this, defaultValueIfNotFound: $defaultValueIfNotFound);
-        }
+    {
+        return First::from(elements: $this, defaultValueIfNotFound: $defaultValueIfNotFound);
+    }
 
     public function flatten(): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: FlatMap::oneLevel()));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: FlatMap::oneLevel()));
+    }
 
     public function getBy(int $index, mixed $defaultValueIfNotFound = null): mixed
-        {
-                    return $this->pipeline->getBy(index: $index, defaultValueIfNotFound: $defaultValueIfNotFound);
-        }
+    {
+        return $this->pipeline->getBy(index: $index, defaultValueIfNotFound: $defaultValueIfNotFound);
+    }
 
     public function groupBy(Closure $classifier): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: GroupInto::by(classifier: $classifier)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: GroupInto::by(classifier: $classifier)));
+    }
 
     public function isEmpty(): bool
-        {
-                    return First::isAbsent(elements: $this);
-        }
+    {
+        return First::isAbsent(elements: $this);
+    }
 
     public function joinToString(string $separator): string
-        {
-                    return Join::elements(elements: $this, separator: $separator);
-        }
+    {
+        return Join::elements(elements: $this, separator: $separator);
+    }
 
     public function last(mixed $defaultValueIfNotFound = null): mixed
-        {
-                    return Last::from(elements: $this, defaultValueIfNotFound: $defaultValueIfNotFound);
-        }
+    {
+        return Last::from(elements: $this, defaultValueIfNotFound: $defaultValueIfNotFound);
+    }
 
     public function map(Closure ...$transformations): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: Map::using(...$transformations)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: Map::using(...$transformations)));
+    }
 
     public function reduce(Closure $accumulator, mixed $initial): mixed
-        {
-                    return Reduce::from(elements: $this, accumulator: $accumulator, initial: $initial);
-        }
+    {
+        return Reduce::from(elements: $this, accumulator: $accumulator, initial: $initial);
+    }
 
     public function sort(Order $order = Order::ASCENDING_KEY, ?Closure $comparator = null): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: Rearrange::by(order: $order, comparator: $comparator)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: Rearrange::by(order: $order, comparator: $comparator)));
+    }
 
     public function slice(int $offset, int $length = -1): static
-        {
-                    return new static(pipeline: $this->pipeline->pipe(operation: Segment::from(offset: $offset, length: $length)));
-        }
+    {
+        return new static(pipeline: $this->pipeline->pipe(operation: Segment::from(offset: $offset, length: $length)));
     }
+}
