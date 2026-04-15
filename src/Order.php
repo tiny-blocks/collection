4804523d42551b489c38b1d2058c12f986cda4f6
Gustavo Freze <gustavo.freze@outlook.com>
2026-04-15 15:58:20 -0300

    fix: Correct indentation in Order

--- src/Order.php
+++ src/Order.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace TinyBlocks\Collection;
 
 enum Order
-    {
-        case ASCENDING_KEY;
-        case DESCENDING_KEY;
-        case ASCENDING_VALUE;
-        case DESCENDING_VALUE;
-    }
+{
+    case ASCENDING_KEY;
+    case DESCENDING_KEY;
+    case ASCENDING_VALUE;
+    case DESCENDING_VALUE;
+}
